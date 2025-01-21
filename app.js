1ec5320a8f9c047802a4953356835312dca19565
speng002 <speng20@yahoo.com>
2025-01-20 23:32:17 -0800

    playing with emit

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function add(a, b){
-    return(a + b)
-};
+// function add(a, b){
+//     return(a + b)
+// };
 
-console.log(add(1, 2))
+// console.log(add(1, 2))
+
+const { EventEmitter } = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('lunch', () => {
+
+    console.log('yum yum')
+})
+
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
